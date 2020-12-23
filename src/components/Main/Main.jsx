fe04854158edaf3284b9eeac25f0fe007ab3d3df
Swapnil Sagar <swapnil.swapnil.sagar946@gmail.com>
2020-12-23 11:33:37 +0530

    Created new index and appjs with main and detail components along with material ui styling

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+
+import useStyles from "./styles";
+
+const Main = () => {
+  const classes = useStyles();
+  return (
+    <div>
+      <Card className={classes.root}>
+        <CardHeader title="Expense Tracker" subheader="By Sagar" />
+        <CardContent>
+          <Typography align="center" variant="h5">
+            Total Balance $100
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            styles={{ lineHeight: "1.5em", marginTop: "20px" }}
+          >
+            Try saying: add income for 100$ in category salary for monday
+          </Typography>
+          <Divider />
+          {/* form */}
+        </CardContent>
+        <CardContent className={classes.cardContent}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              {/* <List/> */}
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default Main;
